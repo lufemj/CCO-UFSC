fecd29ca3f9c31e5d4fefca789e884915c68713c
Luis Fernando Mendonça Junior <50549955+LufeMJ@users.noreply.github.com>
2023-03-31 18:38:42 -0300

    finish ex1

--- AF 2.1 - Processos/Exercicio_1.c
+++ AF 2.1 - Processos/Exercicio_1.c
@@ -26,22 +26,23 @@ int main(int argc, char** argv) {
     pid_t pFilho2;
     pFilho1 = fork();
 
-    if (pFilho1 > 1) {
-        printf("Processo filho %d criado\n", getpid());
-    }
-    else{
+
+    if (pFilho1 == 0) {
         printf("Processo pai criou %d\n", pFilho1);
 
         pFilho2 = fork();
 
-        if (pFilho2 > 0) {
-            printf("Processo filho %d criado\n", getpid());
-        }
-
-        else {
+        if (pFilho2 == 0) {
             printf("Processo pai criou %d\n", pFilho2);
             wait(NULL);
         }
+
+        else {
+            printf("Processo filho %d criado\n", getpid());
+        } 
+    }
+    else {
+        printf("Processo filho %d criado\n", getpid());
     }
 
     if(wait(NULL) < 0){
