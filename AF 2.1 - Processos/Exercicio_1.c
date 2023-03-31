da358f655dd909b3b0210c823b97b9aa7e7f15dd
Luis Fernando Mendonça Junior <50549955+LufeMJ@users.noreply.github.com>
2023-03-31 18:32:46 -0300

    Update Exercicio_1.c

--- AF 2.1 - Processos/Exercicio_1.c
+++ AF 2.1 - Processos/Exercicio_1.c
@@ -4,22 +4,6 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
-// (pai)
-// |
-// +----+----+
-// | |
-// filho_1 filho_2
-
-
-// ~~~ printfs ~~~
-// pai (ao criar filho): "Processo pai criou %d\n"
-// pai (ao terminar): "Processo pai finalizado!\n"
-// filhos (ao iniciar): "Processo filho %d criado\n"
-
-// Obs:
-// - pai deve esperar pelos filhos antes de terminar!
-
-
 int main(int argc, char** argv) {
 
     pid_t pFilho1;
