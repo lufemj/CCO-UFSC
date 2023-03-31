63f095a54f1ac490008712a7f3defab40ee3e451
Luis Fernando Mendonça Junior <50549955+LufeMJ@users.noreply.github.com>
2023-03-31 13:51:20 -0300

    ex1 feito

--- AF 2.1 - Processos/Exercicio_1.c
+++ AF 2.1 - Processos/Exercicio_1.c
@@ -4,17 +4,17 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
-//       (pai)      
-//         |        
-//    +----+----+
-//    |         |   
-// filho_1   filho_2
+// (pai)
+// |
+// +----+----+
+// | |
+// filho_1 filho_2
 
 
-// ~~~ printfs  ~~~
+// ~~~ printfs ~~~
 // pai (ao criar filho): "Processo pai criou %d\n"
-//    pai (ao terminar): "Processo pai finalizado!\n"
-//  filhos (ao iniciar): "Processo filho %d criado\n"
+// pai (ao terminar): "Processo pai finalizado!\n"
+// filhos (ao iniciar): "Processo filho %d criado\n"
 
 // Obs:
 // - pai deve esperar pelos filhos antes de terminar!
@@ -22,35 +22,37 @@
 
 int main(int argc, char** argv) {
 
+    int filho1;
+    filho1 = fork();
+
+    if (filho1 > 0) {
+        printf("Processo filho %d criado\n", filho1);
+
+
+        int filho2;
+        filho2 = fork();
+
+        if (filho2 > 0) {
+            printf("Processo filho %d criado\n", filho2);
+        } 
+        else if (filho2 == 0) {
+            printf("Processo filho criado\n");
+        }
+
+    } 
+    else if (filho1 == 0) {
+        printf("Processo filho criado\n");
+    }
+
     // ....
 
     /*************************************************
-     * Dicas:                                        *
-     * 1. Leia as intruções antes do main().         *
-     * 2. Faça os prints exatamente como solicitado. *
-     * 3. Espere o término dos filhos                *
-     *************************************************/
+    * Dicas: *
+    * 1. Leia as intruções antes do main(). *
+    * 2. Faça os prints exatamente como solicitado. *
+    * 3. Espere o término dos filhos *
+    *************************************************/
 
-    pid_t pFilho1;
-    pFilho1 = fork();
-
-    if (pFilho1 == 0){
-        printf("Processo pai criou %d\n", getpid());
-    }
-    else{
-        wait(NULL);
-    }
-
-    pid_t pFilho2;
-    pFilho2 = fork();
-
-    if (pFilho2 == 0){
-        printf("Processo pai criou %d\n", getpid());
-    }
-    else{
-        wait(NULL);
-    }
-
-    printf("Processo pai finalizado!\n");   
+    printf("Processo pai finalizado!\n");
     return 0;
 }
