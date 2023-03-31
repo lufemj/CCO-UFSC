7e8835e9aa7a8032918ce6c773965a8883e2df62
Luis Fernando Mendonça Junior <50549955+LufeMJ@users.noreply.github.com>
2023-03-31 18:37:31 -0300

    aa

--- AF 2.1 - Processos/Exercicio_1.c
+++ AF 2.1 - Processos/Exercicio_1.c
@@ -4,29 +4,44 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
+// (pai)
+// |
+// +----+----+
+// | |
+// filho_1 filho_2
+
+
+// ~~~ printfs ~~~
+// pai (ao criar filho): "Processo pai criou %d\n"
+// pai (ao terminar): "Processo pai finalizado!\n"
+// filhos (ao iniciar): "Processo filho %d criado\n"
+
+// Obs:
+// - pai deve esperar pelos filhos antes de terminar!
+
+
 int main(int argc, char** argv) {
 
     pid_t pFilho1;
     pid_t pFilho2;
     pFilho1 = fork();
 
-
-    if (pFilho1 == 0) {
+    if (pFilho1 > 1) {
+        printf("Processo filho %d criado\n", getpid());
+    }
+    else{
         printf("Processo pai criou %d\n", pFilho1);
 
         pFilho2 = fork();
 
-        if (pFilho2 == 0) {
-            printf("Processo pai criou %d\n", pFilho2);
-            wait(NULL);
+        if (pFilho2 > 0) {
+            printf("Processo filho %d criado\n", getpid());
         }
 
         else {
-            printf("Processo filho %d criado\n", getpid());
-        } 
-    }
-    else {
-        printf("Processo filho %d criado\n", getpid());
+            printf("Processo pai criou %d\n", pFilho2);
+            wait(NULL);
+        }
     }
 
     if(wait(NULL) < 0){
