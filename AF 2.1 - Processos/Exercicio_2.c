9be0ba7fb1618952814cbe063a925ce193923863
Luis Fernando Mendonça Junior <50549955+LufeMJ@users.noreply.github.com>
2023-04-03 22:20:48 -0300

    ex2 comeco

--- AF 2.1 - Processos/Exercicio_2.c
+++ AF 2.1 - Processos/Exercicio_2.c
@@ -24,16 +24,51 @@
 // - pais devem esperar pelos seu descendentes diretos antes de terminar
 
 int main(int argc, char** argv) {
+    pid_t pid1, pid2, pid_neto;
+    int i;
 
-    // ....
+    // criação dos dois processos filhos
+    pid1 = fork();
+    if (pid1 == 0) { // processo filho 1
+        for (i = 0; i < 3; i++) {
+            pid_neto = fork();
+            if (pid_neto == 0) { // processo neto
+                printf("Processo %d, filho de %d\n", getpid(), getppid());
+                sleep(5);
+                printf("Processo %d finalizado\n", getpid());
+                _exit(0);
+            }
+        }
+        for (i = 0; i < 3; i++) {
+            wait(NULL);
+        }
+        printf("Processo %d finalizado\n", getpid());
+        _exit(0);
+    }
 
-    /*************************************************
-     * Dicas:                                        *
-     * 1. Leia as intruções antes do main().         *
-     * 2. Faça os prints exatamente como solicitado. *
-     * 3. Espere o término dos filhos                *
-     *************************************************/
+    pid2 = fork();
+    if (pid2 == 0) { // processo filho 2
+        for (i = 0; i < 3; i++) {
+            pid_neto = fork();
+            if (pid_neto == 0) { // processo neto
+                printf("Processo %d, filho de %d\n", getpid(), getppid());
+                sleep(5);
+                printf("Processo %d finalizado\n", getpid());
+                _exit(0);
+            }
+        }
+        for (i = 0; i < 3; i++) {
+            wait(NULL);
+        }
+        printf("Processo %d finalizado\n", getpid());
+        _exit(0);
+    }
 
-    printf("Processo principal %d finalizado\n", getpid());    
+    // processo principal
+    printf("Processo principal %d, criou os processos filhos %d e %d\n", getpid(), pid1, pid2);
+    for (i = 0; i < 2; i++) {
+        wait(NULL);
+    }
+    printf("Processo principal %d finalizado\n", getpid());
     return 0;
 }
