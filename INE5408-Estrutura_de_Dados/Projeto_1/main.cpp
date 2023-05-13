f1ae1e211da73d8f1b76408b7cf1e79345725476
Luis Fernando Mendonça Junior <50549955+LufeMJ@users.noreply.github.com>
2023-05-12 21:23:10 -0300

    ed

---
+++ INE5408-Estrutura_de_Dados/Projeto_1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "array_stack.h"
+
+int main() {
+
+    char xmlfilename[100];
+
+    std::cin >> xmlfilename;  // entrada
+    
+    /*
+     
+       COLOQUE SEU CODIGO AQUI
+    
+    */
+
+    std::cout << xmlfilename << std::endl;  // esta linha deve ser removida
+
+    return 0;
+}
