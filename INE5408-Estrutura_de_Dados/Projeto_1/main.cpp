c5ff619eaafd77a19abe8706a45063418c8e0af9
Luis Fernando Mendonça Junior <50549955+LufeMJ@users.noreply.github.com>
2023-05-15 23:15:08 -0300

    ed quase pronto

--- INE5408-Estrutura_de_Dados/Projeto_1/main.cpp
+++ INE5408-Estrutura_de_Dados/Projeto_1/main.cpp
@@ -4,7 +4,89 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include "fila.cpp"
+#include "lista.cpp"
 
+struct Coordenadas {
+    int x;
+    int y;
+    
+    bool operator==(const Coordenadas& other) const {
+        return x == other.x && y == other.y;
+    }
+};
+
+int operacaoRobo(int** matriz_cenario, int** matriz_zero, int altura, int largura, int robo_x, int robo_y) {
+   
+    int casasLimpas = 0;
+    Coordenadas coord{robo_x, robo_y};
+    structures::ArrayQueue<Coordenadas> fila(100);
+    structures::ArrayList<Coordenadas> lista(9999);
+
+    // Inicializa o robo na posição correta da matriz de zeros; 
+    if (matriz_cenario[robo_x][robo_y] == 1) {
+        matriz_zero[robo_x][robo_y] = 1;
+    } else {
+        matriz_zero[robo_x][robo_y] = 0;
+    }
+    lista.push_back(coord);
+    fila.enqueue(coord);
+
+    while(!fila.empty()) {
+
+        // Tira o valor da primeira coordenada da fila, e atribui a uma variável;
+        Coordenadas first = fila.dequeue();
+
+        // Coordenadas dos 4 pontos vizinhos da primeira coordenada da fila;
+        Coordenadas coordRight{first.x + 1, first.y};;
+        Coordenadas coordLeft{first.x - 1, first.y};
+        Coordenadas coordUp{first.x, first.y + 1};
+        Coordenadas coordDown{first.x, first.y - 1};
+
+        if (coordRight.x < altura && coordRight.y >= 0 && coordRight.y < largura) {
+            if (matriz_cenario[coordRight.x][coordRight.y] == 1 && !lista.contains(coordRight)) {
+                matriz_zero[coordRight.x][coordRight.y] = 1;
+                lista.push_back(coordRight);
+                fila.enqueue(coordRight);
+            }
+        }
+
+        if (coordLeft.x >= 0 && coordLeft.y >= 0 && coordLeft.y < largura) {
+            if (matriz_cenario[coordLeft.x][coordLeft.y] == 1 && !lista.contains(coordLeft)) {
+                matriz_zero[coordLeft.x][coordLeft.y] = 1;
+                lista.push_back(coordLeft);
+                fila.enqueue(coordLeft);
+            }
+        }
+
+        if (coordUp.x >= 0 && coordUp.x < altura && coordUp.y >= 0) {
+            if (matriz_cenario[coordUp.x][coordUp.y] == 1 && !lista.contains(coordUp)) {
+                matriz_zero[coordUp.x][coordUp.y] = 1;
+                lista.push_back(coordUp);
+                fila.enqueue(coordUp);
+            }
+        }
+
+
+        if (coordDown.x >= 0 && coordDown.x < altura && coordDown.y >= 0 && coordDown.y < largura) {
+            if (matriz_cenario[coordDown.x][coordDown.y] == 1 && !lista.contains(coordDown)) {
+                matriz_zero[coordDown.x][coordDown.y] = 1;
+                lista.push_back(coordDown);
+                fila.enqueue(coordDown);
+            }
+        }
+    }
+
+    for (int i = 0; i < altura; i++) {
+        for (int j = 0; j < largura; j++) {
+            if (matriz_zero[i][j] == 1) {
+                casasLimpas++;
+            }
+        }
+    }
+
+    return casasLimpas;
+}
 
 bool validacaoArquivo(const std::string& file) {
     /*
@@ -46,12 +128,6 @@ int** matrizGerador(const std::string& matriz_string, int altura, int largura, b
         }
     }
     return matriz;
-}
-
-int operacaoRobo(int** matriz_cenario, int** matriz_zero, int altura, int largura, int robo_x, int robo_y) {
-    int casasLimpas = 0;
-    
-    return casasLimpas;
 }
 
 int main() {
