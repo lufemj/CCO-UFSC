9acd0f46da515db08f3ead463dbf97a50c7401de
Luis Fernando Mendonça Junior <50549955+LufeMJ@users.noreply.github.com>
2023-05-24 14:55:48 -0300

    Update Exercicio_1.asm

--- INE5411-Organização_de_Computadores_I/Atividade_3/Exercicio_1.asm
+++ INE5411-Organização_de_Computadores_I/Atividade_3/Exercicio_1.asm
@@ -3,43 +3,39 @@
 #Atividade 3	Exercício 1
 
 .data
-Matriz:		.space 256		#Aloca espaço da Matriz na memória
+matriz:		.space 256		#Aloca espaço da Matriz na memória
 	  
 
 .text
 MAIN:
-	la	$s0, Matriz		#Carrega o endereço da matriz
-	li	$s1, 0			#Armazena a quantidade de elementos armazenados por linha
+	la	$s0, matriz		#Carrega o endereço da matriz
+	li	$s1, 0			#Armazena o valor do elemento a ser armazenado
+	li	$t0, 0			#Armazena o valor da linha
+	li	$t1, 0			#Armazena o valor da coluna
 	
-LOOP:
+	j	LOOP_COLUNA		#Inicia o Loop
+	
+LOOP_LINHA:
+	addi	$t0, $t0, 1		#Soma 1 no valor da linha
+	beq	$t0, 16, END		#Se finalizar a utlima linha vai para o fim do programa
 
+LOOP_COLUNA:
+	beq	$t1, 16, RESET_COLUNA	#Se finalizar a utlima coluna vai chama a função para resetar o valor
 	
-
-	lw	$t3, ($s1)		#
-	lw	$t4, 12($s1)		#Carrega cada valor da coluna de B
-	lw	$t5, 24($s1)		#
+	mul $t3, $t0, 4			#Multiplica o valor da linha por 4 para encontrar a posição correta no endereço
+	mul $t4, $t1, 64		#Multiplica o valor da coluna por 64 para encontrar a posição correta no endereço
+	add $t3, $t3, $t4		#Soma esse dois valores e armazena o resultado em $t3
+	add $t4, $t3, $s0		#Por fim, soma o resultado ao endereço da matriz para obter a posição atual da matriz
 	
-	mul	$t6, $t0, $t3		#Multiplica o primeiro valor da linha pelo primeiro valor da coluna e armazena o valor em $t6
-	mul	$t7, $t1, $t4		#Multiplica o segundo valor da linha pelo segundo valor da coluna e armazena o valor em $t7
-	add	$t6, $t6, $t7		#Soma o resultado das duas operações e armazena o valor em $t6
-	mul	$t7, $t2, $t5		#Multiplica o terceiro valor da linha pelo terceiro valor da coluna e armazena o valor em $t7
-	add	$t6, $t6, $t7		##Soma o resultado das operação anterior com o resultado antigo e armazena o valor em $t6
-
-	sw	$t6, ($s2)		#Armazenao valor salvo em $t6 na matriz AB
-	addi	$s2, $s2, 4		#Adiciona 4 ao endereço de AB, para armazenar o próximo valor no loop em seu respectivo lugar da matriz
+	sw $s1, 0($t4)			#Armazena o valor atual na posição correta da matriz
+	addi $s1, $s1, 1		#Soma mais um no valor a ser armazenado
 	
-	addi	$s1, $s1, 4		#Adiciona 4 ao endereço de B, para que no proximo loop sejam carregados os valores da próxima coluna
-	addi	$s3, $s3, 1		#Adiciona 1 ao valor de elementos armazenados na linha
-	beq 	$s3, 3, RESET		#Se o número de valores armazenados na linha for igual a 3, vai para a função RESET
-	j	LOOP			#Volta ao loop para calcular os próximos valores da matriz
+	addi	$t1, $t1, 1		#Soma 1 no valor da coluna
+	j	LOOP_COLUNA		#Volta para o loop da coluna
 	
-RESET:
-	addi	$s4, $s4, 3		#Adiciona 3 ao valor de elementos armazenados na matriz
-	beq	$s4, 9, END		#Se o número de valores armazenados na matriz for igual a 9, vai para a função END
-	li	$s3, 0			#Reseta o valor de elementos armazenados na linha de volta para 0
-	addi	$s0, $s0, 12		#Adiciona 12 ao endereço de A, para que no próximo loop sejam carregados os valores da próxima linha
-	la	$s1, B			#ACarrega cada novamente o endereço original de B para realizar novamente as operações
-	j	LOOP			#Volta ao loop para calcular os próximos valores da matriz
+RESET_COLUNA:
+	li	$t1, 0			#Reseta o valor da coluna
+	j	LOOP_LINHA		#Após resetar o valor da coluna ele chama a função para próxima linha
 	
 END:
 	li $v0, 10 			#Chamada do sistema para sair
