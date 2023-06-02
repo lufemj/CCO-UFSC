ec43ed6292af01e2230ec5bd31602ec67f87fdad
Luis Fernando Mendonça Junior <50549955+LufeMJ@users.noreply.github.com>
2023-06-02 10:26:05 -0300

    Programacao Concorrente Python

---
+++ INE5410-Progamacao_Concorrente/AF_3.4-Concorrência_em_Python/ex-1.py
@@ -0,0 +1,40 @@
+from time import sleep
+from random import randint
+from threading import Thread, Lock, Condition
+
+def produtor():
+  global buffer
+  for i in range(10):
+    sleep(randint(0,2))           # fica um tempo produzindo...
+    item = 'item ' + str(i)
+    with lock:
+      if len(buffer) == tam_buffer:
+        print('>>> Buffer cheio. Produtor ira aguardar.')
+        lugar_no_buffer.wait()    # aguarda que haja lugar no buffer
+      buffer.append(item)
+      print('Produzido %s (ha %i itens no buffer)' % (item,len(buffer)))
+      item_no_buffer.notify()
+
+def consumidor():
+  global buffer
+  for i in range(10):
+    with lock:
+      if len(buffer) == 0:
+        print('>>> Buffer vazio. Consumidor ira aguardar.')
+        item_no_buffer.wait()   # aguarda que haja um item para consumir 
+      item = buffer.pop(0)
+      print('Consumido %s (ha %i itens no buffer)' % (item,len(buffer)))
+      lugar_no_buffer.notify()
+    sleep(randint(0,2))         # fica um tempo consumindo...
+
+buffer = []
+tam_buffer = 5
+lock = Lock()
+lugar_no_buffer = Condition(lock)
+item_no_buffer = Condition(lock)
+produtor = Thread(target=produtor) 
+consumidor = Thread(target=consumidor) 
+produtor.start()
+consumidor.start()
+produtor.join()
+consumidor.join() 
---
+++ INE5410-Progamacao_Concorrente/AF_3.4-Concorrência_em_Python/ex-2.py
@@ -0,0 +1,30 @@
+from time import sleep
+from random import randint
+from threading import Thread, Semaphore
+
+def produtor():
+  global buffer
+  for i in range(10):
+    sleep(randint(0,2))           # fica um tempo produzindo...
+    item = 'item ' + str(i)
+    # verifica se há lugar no buffer
+    buffer.append(item)
+    print('Produzido %s (ha %i itens no buffer)' % (item,len(buffer)))
+
+def consumidor():
+  global buffer
+  for i in range(10):
+    # aguarda que haja um item para consumir 
+    item = buffer.pop(0)
+    print('Consumido %s (ha %i itens no buffer)' % (item,len(buffer)))
+    sleep(randint(0,2))         # fica um tempo consumindo...
+
+buffer = []
+tam_buffer = 3
+# cria semáforos
+produtor = Thread(target=produtor) 
+consumidor = Thread(target=consumidor) 
+produtor.start()
+consumidor.start()
+produtor.join()
+consumidor.join() 
