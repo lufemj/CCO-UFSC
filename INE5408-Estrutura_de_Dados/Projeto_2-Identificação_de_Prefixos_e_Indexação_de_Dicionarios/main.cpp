07efe88931ca2676100fae2c086471c899f17420
Luis Fernando Mendonça Junior <50549955+LufeMJ@users.noreply.github.com>
2023-06-30 18:19:16 -0300

    Update main.cpp

--- INE5408-Estrutura_de_Dados/Projeto_2-Identificação_de_Prefixos_e_Indexação_de_Dicionarios/main.cpp
+++ INE5408-Estrutura_de_Dados/Projeto_2-Identificação_de_Prefixos_e_Indexação_de_Dicionarios/main.cpp
@@ -11,13 +11,36 @@ NoTrie {
 }
 
 
+
+// Função parametrizada que retorna a informação desejada do .xml
+void extractDado() {
+    std::string dado;
+    size_t startPos = file.find("<" + tag + ">", pos_cenario);
+    if (startPos != std::string::npos) {
+        startPos = file.find("<" + var + ">", startPos);
+        if (startPos != std::string::npos) {
+            size_t endPos = file.find("</" + var + ">", startPos);
+            if (endPos != std::string::npos){
+                startPos += var.length() + 2;
+                dado = file.substr(startPos, endPos - startPos);
+            }
+        }
+    }
+    return dado;
+}
+
 int main() {
     string filename;
     string word;
 
     cin >> filename;  // entrada
 
-    cout << filename << endl;  // esta linha deve ser removida
+    ifstream arquivo(filename);
+    ostringstream saved_string;
+    saved_string << arquivo.rdbuf();
+    file = saved_string.str();
+
+    extractDado(file)
     
     while (1) {  // leitura das palavras ate' encontrar "0"
         cin >> word;
